601748cffc839a84f3044453298ed1444aa9b0e5
Zach Kaufman <zkaufman@protonmail.com>
2022-06-20 20:42:12 -0400

    Added recording functionality to listener.py and added more info to the README

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Engagement Queue Tracking Algorithm
 ## Setup instructions
+### Unfinished
 NOTE: You must be using Ubuntu 18.04 (The Azure Kinect does not currently support Ubuntu 20.04 or newer, and neither does it's ROS driver)
 1. Install ROS Melodic using [this link](https://wiki.ros.org/melodic/Installation/Ubuntu) and [this link](https://wiki.ros.org/ROS/Tutorials/InstallingandConfiguringROSEnvironment) for setting it up
 2. Install the [Azure Kinect SDK](https://docs.microsoft.com/en-us/azure/kinect-dk/sensor-sdk-download) onto your Linux system. Make sure to follow ALL instructions, there are multiple webpages with instructions required for it to work on Linux.
@@ -7,4 +8,14 @@ NOTE: You must be using Ubuntu 18.04 (The Azure Kinect does not currently suppor
 4. INSERT INFO FOR BOSON
 5. Clone this repo into catkin_ws/src/ and run "catkin_make" in catkin_ws/ 
 
-Unfinished
+## Recording Instructions
+1. In the root of this folder, open a terminal and run "python scripts/listener.py \[Seconds\]" and replace \[Seconds\] with the number of seconds you want the program to run.
+2. Do not ctrl+C the program, this will corrupt the output video file.
+3. The output video will by in the output folder after the recording is finished.
+4. 15fps is what is coded into this script, however, the Azure ROS Driver default is 5. You must change the default in the Azure Kinect ROS Driver driver.launch file to 15. Other options are 5, and 30, make sure the number in this file matches the one in the driver.launch. 
+
+### Known bugs
+1. RGB values are reversed
+2. Output is .avi instead of .mp4
+3. Requires a duration instead of being quit with the keyboard
+4. Program is currently set for recording only, no analysis, no facial detection, no live output
--- scripts/listener.py
+++ scripts/listener.py
@@ -1,13 +1,22 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python
 import rospy
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 from std_msgs.msg import String
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image as Img
 from PIL import Image
+from datetime import datetime
 
+frame = 0
+size = (2048, 1536) # This is from the Azure Kinect /rgb/image_raw topic
+frameRate = 15.0 # This is NOT the default FPS for the Azure Kinect. Azure Kinect defaults on 5, change this in driver.launch in the drivers folder of the Azure Kinect ROS Driver
+now = datetime.now()
+fileName = "output/" + now.strftime("%d_%m_%Y-%H_%M_%S")
+fourcc = cv.VideoWriter_fourcc(*'MJPG')
+vid = cv.VideoWriter(fileName + ".avi", fourcc, frameRate, size, True)
 
 def visualCallback(data):
     # Converting from Azure image to OpenCV image
@@ -18,23 +27,35 @@ def visualCallback(data):
     img_grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # Loading Haar Cascade classifier for frontal face
-    haar_frontal_face = cv.CascadeClassifier(cv.data.haarcascades + 'include/haarcascade_frontalface_default.xml')
+    #haar_frontal_face = cv.CascadeClassifier(cv.data.haarcascades + 'include/haarcascade_frontalface_default.xml')
     # Setting face square parameters
-    face_rects = haar_frontal_face.detectMultiScale(img_grey, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30), flags=cv.CASCADE_SCALE_IMAGE)
+    #face_rects = haar_frontal_face.detectMultiScale(img_grey, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30), flags=cv.CASCADE_SCALE_IMAGE)
     # Print number of faces found
-    print("Detected faces: ", len(face_rects))
+    #print("Detected faces: ", len(face_rects))
 
     # Draw rectangules over images
-    for (nx, ny, x, y) in face_rects:
-        cv.rectangle(img, (nx, ny), (nx + x, ny + y), (0, 0, 255), 2)
+    #for (nx, ny, x, y) in face_rects:
+    #    cv.rectangle(img, (nx, ny), (nx + x, ny + y), (0, 0, 255), 2)
     cv.imshow("Camera Output", img)
     cv.waitKey(1)
 
+def visualRecorder(data):
+    global frame
+    # Converting from Azure image to OpenCV image
+    if(vid.isOpened() and frame < (int(sys.argv[1]) * int(frameRate))):
+        bridge = CvBridge()
+        img = bridge.imgmsg_to_cv2(data)
+        vid.write(img)
+        frame += 1
+    else:
+        print("Video recorder turned off. Outputting file.")
+        vid.release()
+
 
 def listener():
-
+    inter = 0
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("/rgb/image_raw", Img, visualCallback)
+    rospy.Subscriber("/rgb/image_raw", Img, visualRecorder)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
