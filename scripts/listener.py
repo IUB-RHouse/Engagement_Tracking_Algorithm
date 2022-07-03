d04aa354a5de9ae0d9aa31901144192724a5d331
Zachary Kaufman <zckaufman@Zacharys-MacBook-Pro-2.local>
2022-07-02 23:10:50 -0400

    Code cleanup and added argument handler

--- scripts/listener.py
+++ scripts/listener.py
@@ -10,14 +10,53 @@ from sensor_msgs.msg import Image as Img
 from PIL import Image
 from datetime import datetime
 
-frame = 0
-duration = False
-size = (2048, 1536) # This is from the Azure Kinect /rgb/image_raw topic
-frameRate = 15.0 # This is NOT the default FPS for the Azure Kinect. Azure Kinect defaults on 5, change this in driver.launch in the drivers folder of the Azure Kinect ROS Driver
+### General Global Variables
 now = datetime.now()
-fileName = "output/" + now.strftime("%d_%m_%Y-%H_%M_%S")
-fourcc = cv.VideoWriter_fourcc(*'MJPG')
-vid = cv.VideoWriter(fileName + ".avi", fourcc, frameRate, size, True)
+fourCC = cv.VideoWriter_fourcc(*'MJPG')
+recVis = False
+recTherm = False
+analyze = False
+
+### Azure Global Variables
+vSize = (2048, 1536) # This is from the Azure Kinect /rgb/image_raw topic
+vFrameRate = 15.0 # This is NOT the default FPS for the Azure Kinect. Azure Kinect defaults on 5, change this in driver.launch in the drivers folder of the Azure Kinect ROS Driver
+vFilename = "output/" + now.strftime("%d_%m_%Y-%H_%M_%S") + "_RGB"
+vVid = cv.VideoWriter(vFilename + ".avi", fourCC, vFrameRate, vSize, True)
+
+### Boson Global Variables
+tSize = (320, 256) # This is from the Boson320 /rgb/image_raw topic
+tFrameRate = 60.0
+tFilename = "output/" + now.strftime("%d_%m_%Y-%H_%M_%S") + "_THERMAL"
+tVid = cv.VideoWriter(tFilename + ".avi", fourCC, tFrameRate, tSize, True)
+
+def argHandler():
+    global recVis
+    global recTherm
+    global analyze
+    global vFrameRate
+    global vVid
+    global fourCC
+    global vSize
+
+    for arg in sys.argv:
+        if(arg[0] == '-'):
+            if(arg[1:2] == "ra"):
+                recVis = True
+                recTherm = True
+            if(arg[1:2] == "rv")
+                recVis = True
+            if(arg[1:2] == "rt"):
+                recTherm = True
+            if(arg[1] == "a"):
+                analyze = True
+        else:
+            if(arg[0:4] == "fps="):
+                vFrameRate = arg[6:7]
+                vVid = cv.VideoWriter(vFilename + ".avi", fourCC, vFrameRate, vSize, True)
+
+
+
+
 
 def endProgram():
     print("Shutdown initiated. Ending program.")
@@ -38,35 +77,59 @@ def visualAnalysis(data):
     cv.waitKey(1)
 
 def visualRecorder(data):
-    global frame
-    global duration
-    # Converting from Azure image to OpenCV image
+    global aFrame
+    # Converting from Azure image to OpenCV image and records it
     if(vid.isOpened()):
-        if(not duration or frame < (int(sys.argv[2]) * int(frameRate))):
+        if(not aDuration or aFrame < (int(sys.argv[2]) * int(vFrameRate))):
             bridge = CvBridge()
             img = bridge.imgmsg_to_cv2(data)
             img = cv.cvtColor(img, cv.COLOR_BGRA2BGR)
             vid.write(img)
-            frame += 1
+            vFrame += 1
         else:
-            print("Set duration reached.")
+            print("Set aDuration reached.")
             vid.release()
     else:
         rospy.signal_shutdown("OpenCV VideoWriter is no longer open. Program ending.")
 
+def visualRecorder(data):
+    # Converting from Boson thermal image to OpenCV image and records it
+    if(vid.isOpened()):
+        bridge = CvBridge()
+        img = bridge.imgmsg_to_cv2(data)
+        img = cv.cvtColor(img, cv.COLOR_BGRA2BGR)
+        vVid.write(img)
+    else:
+        rospy.signal_shutdown("OpenCV VideoWriter is no longer open. Program ending.")
 
-def listener(setDuration):
-    global duration 
-    duration = setDuration
+def thermalRecorder(data):
+    # Converting from Boson thermal image to OpenCV image and records it
+    if(vid.isOpened()):
+        bridge = CvBridge()
+        img = bridge.imgmsg_to_cv2(data)
+        img = cv.cvtColor(img, cv.COLOR_BGRA2BGR)
+        tVid.write(img)
+    else:
+        rospy.signal_shutdown("OpenCV VideoWriter is no longer open. Program ending.")
+
+
+def listener():
+    global recVis
+    global recTherm
+    global analyze
+
     rospy.init_node('listener', anonymous=True)
     rospy.on_shutdown(endProgram)
 
-    if(sys.argv[1] == "-r"):
+    if(recVis):
         rospy.Subscriber("/rgb/image_raw", Img, visualRecorder)
-    elif(sys.argv[1] == "-a"):
+    if(recTherm):
+        rospy.Subscriber("/flir_boson/image_raw", Img, thermalRecorder)
+    if(analyze):
         rospy.Subscriber("/rgb/image_raw", Img, visualAnalysis)
+        rospy.Subscriber("/flir_boson/image_raw", Img, visualAnalysis)
     else:
-        print("Error: Argument \'" + sys.argv[1] + "\' not recognized.")
+        print("Error: Argument(s) not recognized.")
         sys.exit()
 
     # spin() simply keeps python from exiting until this node is stopped
@@ -77,10 +140,6 @@ if __name__ == '__main__':
     if(len(sys.argv) < 2):
         print("Error: Mode argument required")
         sys.exit()
-    elif(sys.argv[1][0] != '-'):
-        print("Error: Argument \'" + sys.argv[1] + "\' is not recognized. Please enter valid mode.")
     else:
-        if(len(sys.argv) > 2):
-            listener(True)
-        else:
-            listener(False)
+        argHandler()
+        listener()
